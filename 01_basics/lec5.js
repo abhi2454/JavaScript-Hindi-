7c68bd4bc8e00484fbe21c4f451494f1e08fe2c8
Abhinav Gupta <76719087+abhi2454@users.noreply.github.com>
2025-08-30 07:30:51 +0000

    datatypes

---
+++ 01_basics/lec5.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+let name = "Abhinav"
+let age = 19
+let isLoggedIn = true
+let state = null
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof undefined) // type -> undefined
+console.log(typeof null) // type -> object
