26b33b996e97e4e446b1097443856d5590ca02be
Abhinav Gupta <76719087+abhi2454@users.noreply.github.com>
2025-09-03 15:28:31 +0000

    conversion confusions

---
+++ 01_basics/lec7.js
@@ -0,0 +1,36 @@
+// ************** Operations **************
+
+let value = 3
+let negValue = -value
+// console.log(negValue); ---> -3
+
+// console.log(2+2); ---> 4
+// console.log(2-2); ---> 0
+// console.log(2*2); ---> 4
+// console.log(2**3); ---> 8
+// console.log(2/3); ---> 0.6666
+// console.log(2%3); ---> 2
+
+let str1 = "Hello"
+let str2 = " Abhinav"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+
+// console.log("1" + 2); --> 12
+// console.log(1 + "2"); --> 12
+// console.log("1" + "2");  --> 12
+// console.log("1" + 2 + 2); --> 122
+// console.log(1 + 2 + "2"); --> 32
+// console.log("2"+ 1 + "2"); --> 212
+// console.log( 1 + 2 + "2" + "2"); --> 322
+
+// console.log(+true); --> conversion bool to num  --> 1 --> makes code complex ( bad practice)
+// console.log(+""); --> conversion string to num --> 0 --> makes code complex and reduces readability
+
+
+
+
+
